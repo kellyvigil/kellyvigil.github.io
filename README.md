cc44e1577e254df4c8bdff0b9d243a92b46df103
Kelly <programmingstation05@Programming-Station-05-iMac-2.local>
2016-10-26 16:23:48 -0400

    Swap out for wwtje

--- README.md
+++
@@ -1,4 +0,0 @@
-Mixes_01
-========
-
-This is a website with mixes I enjoy and photos of the artist(s). 
---
+++ WhenWillThisJobEnd.js
@@ -0,0 +1,35 @@
+CountDownTimer('10/31/2016 10:1 AM', 'countdown');
+    function CountDownTimer(dt, id)
+    {
+        var end = new Date(dt);
+
+        var _second = 1000;
+        var _minute = _second * 60;
+        var _hour = _minute * 60;
+        var _day = _hour * 24;
+        var timer;
+
+        function showRemaining() {
+            var now = new Date();
+            var distance = end - now;
+            if (distance < 0) {
+
+                clearInterval(timer);
+                document.getElementById(id).innerHTML = 'SHOT N A BEER TIME!';
+
+                return;
+            }
+            var days = Math.floor(distance / _day);
+            var hours = Math.floor((distance % _day) / _hour);
+            var minutes = Math.floor((distance % _hour) / _minute);
+            var seconds = Math.floor((distance % _minute) / _second);
+
+            document.getElementById("days").innerHTML = days + " ";
+            document.getElementById("hours").innerHTML = hours + " ";
+            document.getElementById("minutes").innerHTML = minutes + " ";
+            document.getElementById("seconds").innerHTML = seconds + " ";
+
+        }
+
+        timer = setInterval(showRemaining, 1000);
+    }
